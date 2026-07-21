6db654fafd78513d3aeb006e57f967f0b6b1919f
Aika336 <vladock1999Suka@gmail.com>
2026-07-21 03:25:54 +0200

    delete unused header files

--- MemDumpUser/header/ReadProcessMemoryW.h
+++ MemDumpUser/header/ReadProcessMemoryW.h
@@ -1,6 +1,4 @@
-#include "DeviceRaii.h"
-#include "IoCtlCodes.h"
-#include "Requests.h"
+#include <Windows.h>
 
 NTSTATUS ReadProcessMemoryW(const char* targetName, LPVOID address, LPVOID outBuffer, int size);
 NTSTATUS ReadProcessMemoryW(int pId, LPVOID address, LPVOID outBuffer, int size);
