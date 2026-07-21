876dae8e2273bdae15da1696bc862c865723e66b
Aika336 <vladock1999Suka@gmail.com>
2026-07-21 03:26:47 +0200

    feat: implements memory reader by target process name

---
+++ MemDumpUser/src/ReadProcessMemoryW.cpp
@@ -0,0 +1,33 @@
+#include "../header/ReadProcessMemoryW.h"
+#include "../header/DeviceRaii.h"
+#include "../header/IoCtlCodes.h"
+#include "../header/Requests.h"
+
+#include <stdexcept>
+#include <string>
+#include <ntstatus.h>
+
+NTSTATUS ReadProcessMemoryW(const char* targetName, LPVOID address, LPVOID outBuffer, int size) {
+	if (!address || !outBuffer || size < 0) {
+		return STATUS_INVALID_PARAMETER;
+	}
+
+	DeviceRaii device;
+
+	_READ_REQUEST request{ (UCHAR*)targetName, NULL, (ULONG_PTR)address, size};
+	DWORD bytesRet = 0;
+
+	BOOL state = DeviceIoControl(
+		device.hDevice,
+		IOCTL_READ_MEMORY_BY_NAME,
+		&request, sizeof(request),
+		&outBuffer, sizeof(LPVOID),
+		&bytesRet,
+		NULL
+	);
+	if (!state) {
+		return STATUS_FATAL_APP_EXIT;
+	}
+	
+	return STATUS_SUCCESS;
+}
