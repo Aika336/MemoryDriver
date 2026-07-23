96ce21c0bc7c965106153962eea08dd7b254e0f5
Aika336 <vladock1999Suka@gmail.com>
2026-07-23 16:06:37 +0200

    feat: implement write data in a process by his process id

--- MemDump/MemDriver/WriteMemory/MemoryWriter.c
+++ MemDump/MemDriver/WriteMemory/MemoryWriter.c
@@ -26,3 +26,30 @@ NTSTATUS NameWriteRequest(PCHAR targetName, PVOID address, PVOID writeData, ULON
 
 	return status;
 }
+
+NTSTATUS HandleWriteRequest(ULONG processId, PVOID address, PVOID writeData, ULONG size) {
+	PEPROCESS process;
+
+	NTSTATUS status = PsLookupProcessByProcessId((HANDLE)processId, &process);
+
+	if (!NT_SUCCESS(status)) {
+		return status;
+	}
+
+	KAPC_STATE apcState;
+	KeStackAttachProcess(process, &apcState);
+
+	__try {
+		ProbeForRead(address, size, 1);
+		RtlCopyMemory(address, writeData, size);
+		status = STATUS_SUCCESS;
+	}
+	__except (EXCEPTION_EXECUTE_HANDLER) {
+		status = GetExceptionCode();
+	}
+
+	KeUnstackDetachProcess(&apcState);
+	ObDereferenceObject(process);
+
+	return status;
+}
