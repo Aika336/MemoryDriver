473bfb4645bacb6bef788cb20f6a1bb1fe08da29
Aika336 <vladock1999Suka@gmail.com>
2026-07-23 15:09:22 +0200

    transferring the required function to the directory for interaction with processes

--- MemDump/MemDriver/ProcessManager/ProcessFinder.h
+++ MemDump/MemDriver/ProcessManager/ProcessFinder.h
@@ -1,3 +1,9 @@
 #include <ntddk.h>
 
+NTKERNELAPI
+PCHAR
+PsGetProcessImageFileName(
+    _In_ PEPROCESS Process
+);
+
 PEPROCESS FindProcessByName(UCHAR targetName);
--- MemDump/MemDriver/ReadMemory/MemoryReader.h
+++ MemDump/MemDriver/ReadMemory/MemoryReader.h
@@ -1,11 +1,5 @@
 #pragma once
 #include "../header/driver.h"
 
-NTKERNELAPI
-PCHAR
-PsGetProcessImageFileName(
-    _In_ PEPROCESS Process
-);
-
 NTSTATUS NameReadRequest(PCHAR targetName, PVOID address, ULONG size, PVOID outBuffer);
 NTSTATUS HandleReadRequest(ULONG pid, PVOID address, ULONG size, PVOID outBuffer);
