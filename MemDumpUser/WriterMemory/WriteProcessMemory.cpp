4d098d86cfb9f00ba794dd3dca089d71adab8293
Aika336 <vladock1999Suka@gmail.com>
2026-07-23 17:53:51 +0200

    feat: A function has been implemented for writing data to a process provided through his process id

--- MemDumpUser/WriterMemory/WriteProcessMemory.cpp
+++ MemDumpUser/WriterMemory/WriteProcessMemory.cpp
@@ -32,3 +32,30 @@ NTSTATUS WriteProcessMemoryW(const char* targetName, LPVOID targetAddress, LPVOI
 
 	return STATUS_SUCCESS;
 }
+
+NTSTATUS WriteProcessMemoryW(ULONG processId, LPVOID targetAddress, LPVOID dataBuffer, int size) {
+	if (!targetAddress || size < 0) {
+		return STATUS_INVALID_PARAMETER;
+	}
+
+	DeviceRaii device;
+
+	WRITE_REQUEST request{ NULL , processId, (ULONG_PTR)targetAddress, (ULONG_PTR)dataBuffer, size };
+
+	DWORD bytesRet;
+
+	BOOL state = DeviceIoControl(
+		device.hDevice,
+		IOCTL_READ_MEMORY_BY_NAME,
+		&request, sizeof(request),
+		NULL, 0,
+		&bytesRet,
+		NULL
+	);
+
+	if (!state) {
+		return STATUS_FATAL_APP_EXIT;
+	}
+
+	return STATUS_SUCCESS;
+}
