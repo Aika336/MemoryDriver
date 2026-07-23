7259d2cc22b8f874144bb8dd73e48bb8fd954247
Aika336 <vladock1999Suka@gmail.com>
2026-07-23 17:52:45 +0200

    feat: A function has been implemented for writing data to a process provided through his name.

--- MemDumpUser/WriterMemory/WriteProcessMemory.cpp
+++ MemDumpUser/WriterMemory/WriteProcessMemory.cpp
@@ -1,5 +1,34 @@
 #include "WriteProcessMemory.h"
 
-NTSTATUS WriteProcessMemoryW(const char* targetName, LPVOID targetAddress, LPVOID dataBuffer, int size) {
+#include "../Driver/DeviceRaii.h"
+#include "../Driver/IoCtlCodes.h"
+#include "../Driver/Requests.h"
 
+#include <ntstatus.h>
+
+NTSTATUS WriteProcessMemoryW(const char* targetName, LPVOID targetAddress, LPVOID dataBuffer, int size) {
+	if (!targetName || !targetAddress || size < 0 || strlen(targetName) > 15) {
+		return STATUS_INVALID_PARAMETER;
+	}
+
+	DeviceRaii device;
+
+	WRITE_REQUEST request{(char*)targetName , NULL, (ULONG_PTR)targetAddress, (ULONG_PTR)dataBuffer, size};
+
+	DWORD bytesRet;
+
+	BOOL state = DeviceIoControl(
+		device.hDevice,
+		IOCTL_READ_MEMORY_BY_NAME,
+		&request, sizeof(request),
+		NULL, 0,
+		&bytesRet,
+		NULL
+	);
+
+	if (!state) {
+		return STATUS_FATAL_APP_EXIT;
+	}
+
+	return STATUS_SUCCESS;
 }
