997a3ca6ad45d907af9188fd19d42e7e2d2de0e4
Aika336 <vladock1999Suka@gmail.com>
2026-07-23 16:29:50 +0200

    adding request structure codes for send a writing request to the driver

--- MemDumpUser/Driver/Requests.h
+++ MemDumpUser/Driver/Requests.h
@@ -6,3 +6,11 @@ typedef struct READ_REQUEST {
 	ULONG_PTR  Address;
 	ULONG Size;
 } READ_REQUEST, * PREAD_REQUEST;
+
+typedef struct WRITE_REQUEST {
+	PCHAR targetName;
+	ULONG handle;
+	ULONG_PTR targetAddress;
+	ULONG_PTR dataBuffer;
+	ULONG Size;
+} WRITE_REQUEST, * PWRITE_REQUEST;
