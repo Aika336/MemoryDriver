dd4ad15c3ee7d120cfb46c746e822a458d983e89
Aika336 <vladock1999Suka@gmail.com>
2026-07-25 14:54:15 +0200

    The approach to finding a file name has been changed, in which there is no 15-character limitation on the name of the file being used.

--- MemDump/MemDriver/ProcessManager/ProcessFinder.c
+++ MemDump/MemDriver/ProcessManager/ProcessFinder.c
@@ -3,8 +3,15 @@
 
 PEPROCESS FindProcessByName(PCHAR targetName) {
     const int pidDevider = 4;
-    const int maxProcessName = 15;
     int maxProcessIdes = Exponent(2, 16);
+
+    ANSI_STRING ansi;
+    UNICODE_STRING targeNameUnicode;
+    RtlInitAnsiString(&ansi, targetName);
+
+    if (!NT_SUCCESS(RtlAnsiStringToUnicodeString(&targeNameUnicode, &ansi, TRUE))) {
+        return NULL;
+    }
 
     for (int i = pidDevider; i <= maxProcessIdes; i += pidDevider) {
         PEPROCESS candidate = NULL;
@@ -14,14 +21,31 @@ PEPROCESS FindProcessByName(PCHAR targetName) {
             continue;
         }
 
-        PCHAR processName = PsGetProcessImageFileName(candidate);
+        PUNICODE_STRING fullPath = NULL;
+        status = SeLocateProcessImageName(candidate, &fullPath);
 
-        if (processName && strncmp(processName, targetName, maxProcessName) == 0) {
-            return candidate;
+        if (NT_SUCCESS(status) && fullPath && fullPath->Length > 0) {
+            UNICODE_STRING fileName = *fullPath;
+            for (int j = fileName.Length / sizeof(WCHAR) - 1; j >= 0; j--) {
+                if (fileName.Buffer[j] == L'\\') {
+                    fileName.Buffer += (USHORT)(j + 1);
+                    fileName.Length -= (USHORT)((j + 1) * sizeof(WCHAR));
+                    break;
+                }
+            }
+
+            if (RtlEqualUnicodeString(&targeNameUnicode, &fileName, TRUE)) {
+                ExFreePool(fullPath);
+                RtlFreeUnicodeString(&targeNameUnicode);
+                return candidate;
+            }
         }
 
+        ExFreePool(fullPath);
         ObDereferenceObject(candidate);
     }
 
+    RtlFreeUnicodeString(&targeNameUnicode);
+    
     return NULL;
 }
